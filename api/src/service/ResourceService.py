4481608d4fef99346ec56836ce5d4783892d9217
samuel.jansen <samuel.jansenn@gmail.com>
2023-09-18 17:59:30 -0300

    fix: credit-card share resouce bug fixed

--- api/src/service/ResourceService.py
+++ api/src/service/ResourceService.py
@@ -1,3 +1,4 @@
+from python_helper import ObjectHelper
 from python_framework import Service, ServiceMethod
 
 from dto import AuthorizationAccessDto, CreditCardDto, PurchaseDto
@@ -5,6 +6,16 @@ from dto import AuthorizationAccessDto, CreditCardDto, PurchaseDto
 
 @Service()
 class ResourceService:
+
+
+    @ServiceMethod(requestClass=[[CreditCardDto.CreditCardResponseDto]])
+    def getPurchases(self, creditCardResponseDto):
+        try:
+            return self.service.purchase.findAllByQuery(
+                PurchaseDto.PurchaseQueryAllDto(creditCardKeyList = [creditCardResponseDto.key])
+            )
+        except:
+            return []
 
 
     @ServiceMethod(requestClass=[[AuthorizationAccessDto.AuthorizationAccessAllRequestDto]])
@@ -18,23 +29,23 @@ class ResourceService:
                     self.service.security.shareResource(creditCardResponseDto.credit.key, 'Credit', operation, dto.accountKey, authorizationAccount)
                     self.service.security.shareResource(creditCardResponseDto.key, 'CreditCard', operation, dto.accountKey, authorizationAccount)
             if shareRelatedDomains:
-                self.shareAllPurchase(
-                    [
-                        AuthorizationAccessDto.AuthorizationAccessAllRequestDto(
-                            resourceKeyList = [
-                                purchaseResponseDto.key 
-                                for innerCreditCardResponseDto in creditCardResponseDtoList
-                                for purchaseResponseDto in self.service.purchase.findAllByQuery(
-                                    PurchaseDto.PurchaseQueryAllDto(creditCardKeyList = [innerCreditCardResponseDto.key])
-                                )
-                            ],
-                            domain = dto.domain,
-                            operationList = [*dto.operationList],
-                            accountKey = dto.accountKey
-                        )
-                    ],
-                    shareRelatedDomains = False
-                )
+                resourceKeyList = list(set([
+                    purchaseResponseDto.key 
+                    for innerCreditCardResponseDto in creditCardResponseDtoList
+                    for purchaseResponseDto in self.getPurchases(innerCreditCardResponseDto)
+                ]))
+                if ObjectHelper.isNotEmpty(resourceKeyList):
+                    self.shareAllPurchase(
+                        [
+                            AuthorizationAccessDto.AuthorizationAccessAllRequestDto(
+                                resourceKeyList = [*resourceKeyList],
+                                domain = dto.domain,
+                                operationList = [*dto.operationList],
+                                accountKey = dto.accountKey
+                            )
+                        ],
+                        shareRelatedDomains = False
+                    )
         self.service.security.unlockTransaction(transactionKey)
 
 
@@ -43,38 +54,26 @@ class ResourceService:
         transactionKey = self.service.security.lockTransaction()
         authorizationAccount = self.service.security.getAuthorizationAccount()
         for dto in dtoList:
-            purchaseResponseDtoList = self.service.purchase.findAllByQuery(PurchaseDto.PurchaseQueryAllDto(keyList = dto.resourceKeyList))
-            if shareRelatedDomains:
-                # for creditCardKey in list(set([purchaseResponseDto.creditCardKey for purchaseResponseDto in purchaseResponseDtoList])):
-                #     self.shareAllCreditCard(
-                #         [
-                #             AuthorizationAccessDto.AuthorizationAccessRequestDto(
-                #                 resourceKey = creditCardKey,
-                #                 domain = dto.domain,
-                #                 operation = operation,
-                #                 accountKey = dto.accountKey
-                #             )
-                #             for operation in dto.operationList
-                #         ], 
-                #         shareRelatedDomains = False
-                #     )
-                self.shareAllCreditCard(
-                    [
-                        AuthorizationAccessDto.AuthorizationAccessAllRequestDto(
-                            resourceKeyList = list(set([purchaseResponseDto.creditCardKey for purchaseResponseDto in purchaseResponseDtoList])),
-                            domain = dto.domain,
-                            operationList = dto.operationList,
-                            accountKey = dto.accountKey
-                        )
-                    ], 
-                    shareRelatedDomains = False
-                )
-            for purchaseResponseDto in purchaseResponseDtoList:
-                for installmentResponseDto in purchaseResponseDto.installmentList:
+            if ObjectHelper.isNotEmpty(dto.resourceKeyList):
+                purchaseResponseDtoList = self.service.purchase.findAllByQuery(PurchaseDto.PurchaseQueryAllDto(keyList = dto.resourceKeyList))
+                if shareRelatedDomains:
+                    self.shareAllCreditCard(
+                        [
+                            AuthorizationAccessDto.AuthorizationAccessAllRequestDto(
+                                resourceKeyList = list(set([purchaseResponseDto.creditCardKey for purchaseResponseDto in purchaseResponseDtoList])),
+                                domain = dto.domain,
+                                operationList = dto.operationList,
+                                accountKey = dto.accountKey
+                            )
+                        ], 
+                        shareRelatedDomains = False
+                    )
+                for purchaseResponseDto in purchaseResponseDtoList:
+                    for installmentResponseDto in purchaseResponseDto.installmentList:
+                        for operation in dto.operationList:
+                            self.service.security.shareResource(installmentResponseDto.key, 'Installment', operation, dto.accountKey, authorizationAccount)
                     for operation in dto.operationList:
-                        self.service.security.shareResource(installmentResponseDto.key, 'Installment', operation, dto.accountKey, authorizationAccount)
-                for operation in dto.operationList:
-                    self.service.security.shareResource(purchaseResponseDto.key, 'Purchase', operation, dto.accountKey, authorizationAccount)
+                        self.service.security.shareResource(purchaseResponseDto.key, 'Purchase', operation, dto.accountKey, authorizationAccount)
         self.service.security.unlockTransaction(transactionKey)
 
 
@@ -89,23 +88,23 @@ class ResourceService:
                     self.service.security.shareResource(creditCardResponseDto.credit.key, 'Credit', operation, dto.accountKey, authorizationAccount)
                     self.service.security.shareResource(creditCardResponseDto.key, 'CreditCard', operation, dto.accountKey, authorizationAccount)
             if shareRelatedDomains:
-                self.transferAllPurchase(
-                    [
-                        AuthorizationAccessDto.AuthorizationAccessAllRequestDto(
-                            resourceKeyList = [
-                                purchaseResponseDto.key 
-                                for innerCreditCardResponseDto in creditCardResponseDtoList
-                                for purchaseResponseDto in self.service.purchase.findAllByQuery(
-                                    PurchaseDto.PurchaseQueryAllDto(creditCardKeyList = [innerCreditCardResponseDto.key])
-                                )
-                            ],
-                            domain = dto.domain,
-                            operationList = [*dto.operationList],
-                            accountKey = dto.accountKey
-                        )
-                    ],
-                    shareRelatedDomains = False
-                )
+                resourceKeyList = list(set([
+                    purchaseResponseDto.key 
+                    for innerCreditCardResponseDto in creditCardResponseDtoList
+                    for purchaseResponseDto in self.getPurchases(innerCreditCardResponseDto)
+                ]))
+                if ObjectHelper.isNotEmpty(resourceKeyList):
+                    self.transferAllPurchase(
+                        [
+                            AuthorizationAccessDto.AuthorizationAccessAllRequestDto(
+                                resourceKeyList = [*resourceKeyList],
+                                domain = dto.domain,
+                                operationList = [*dto.operationList],
+                                accountKey = dto.accountKey
+                            )
+                        ],
+                        shareRelatedDomains = False
+                    )
             for creditCardResponseDto in creditCardResponseDtoList:
                 for operation in dto.operationList:
                     self.service.security.revokeResourceAccess(creditCardResponseDto.credit.key, authorizationAccount.key, authorizationAccount)
@@ -118,38 +117,26 @@ class ResourceService:
         transactionKey = self.service.security.lockTransaction()
         authorizationAccount = self.service.security.getAuthorizationAccount()
         for dto in dtoList:
-            purchaseResponseDtoList = self.service.purchase.findAllByQuery(PurchaseDto.PurchaseQueryAllDto(keyList = dto.resourceKeyList))
-            if shareRelatedDomains:
-                # for creditCardKey in list(set([purchaseResponseDto.creditCardKey for purchaseResponseDto in purchaseResponseDtoList])):
-                #     self.shareAllCreditCard(
-                #         [
-                #             AuthorizationAccessDto.AuthorizationAccessRequestDto(
-                #                 resourceKey = creditCardKey,
-                #                 domain = dto.domain,
-                #                 operation = operation,
-                #                 accountKey = dto.accountKey
-                #             )
-                #             for operation in dto.operationList
-                #         ], 
-                #         shareRelatedDomains = False
-                #     )
-                self.shareAllCreditCard(
-                    [
-                        AuthorizationAccessDto.AuthorizationAccessAllRequestDto(
-                            resourceKeyList = list(set([purchaseResponseDto.creditCardKey for purchaseResponseDto in purchaseResponseDtoList])),
-                            domain = dto.domain,
-                            operationList = dto.operationList,
-                            accountKey = dto.accountKey
-                        )
-                    ], 
-                    shareRelatedDomains = False
-                )
-            for purchaseResponseDto in purchaseResponseDtoList:
-                for installmentResponseDto in purchaseResponseDto.installmentList:
+            if ObjectHelper.isNotEmpty(dto.resourceKeyList):
+                purchaseResponseDtoList = self.service.purchase.findAllByQuery(PurchaseDto.PurchaseQueryAllDto(keyList = dto.resourceKeyList))
+                if shareRelatedDomains:
+                    self.shareAllCreditCard(
+                        [
+                            AuthorizationAccessDto.AuthorizationAccessAllRequestDto(
+                                resourceKeyList = list(set([purchaseResponseDto.creditCardKey for purchaseResponseDto in purchaseResponseDtoList])),
+                                domain = dto.domain,
+                                operationList = dto.operationList,
+                                accountKey = dto.accountKey
+                            )
+                        ], 
+                        shareRelatedDomains = False
+                    )
+                for purchaseResponseDto in purchaseResponseDtoList:
+                    for installmentResponseDto in purchaseResponseDto.installmentList:
+                        for operation in dto.operationList:
+                            self.service.security.shareResource(installmentResponseDto.key, 'Installment', operation, dto.accountKey, authorizationAccount)
+                    self.service.security.revokeResourceAccess(installmentResponseDto.key, authorizationAccount.key, authorizationAccount)
                     for operation in dto.operationList:
-                        self.service.security.shareResource(installmentResponseDto.key, 'Installment', operation, dto.accountKey, authorizationAccount)
-                self.service.security.revokeResourceAccess(installmentResponseDto.key, authorizationAccount.key, authorizationAccount)
-                for operation in dto.operationList:
-                    self.service.security.shareResource(purchaseResponseDto.key, 'Purchase', operation, dto.accountKey, authorizationAccount)
-                self.service.security.revokeResourceAccess(purchaseResponseDto.key, authorizationAccount.key, authorizationAccount)
+                        self.service.security.shareResource(purchaseResponseDto.key, 'Purchase', operation, dto.accountKey, authorizationAccount)
+                    self.service.security.revokeResourceAccess(purchaseResponseDto.key, authorizationAccount.key, authorizationAccount)
         self.service.security.unlockTransaction(transactionKey)
